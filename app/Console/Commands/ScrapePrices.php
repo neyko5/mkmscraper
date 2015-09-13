ec4d77db89f7c7b7dda8611b7795282859af69a1
Jernej Lipovec <neykomir@gmail.com>
2015-09-13 22:00:49 +0200

    scrape fix

--- app/Console/Commands/ScrapePrices.php
+++ app/Console/Commands/ScrapePrices.php
@@ -28,10 +28,10 @@ class ScrapePrices extends Command
     public function handle()
     {
         foreach(\MkmScraper\Card::all() as $card){
-            $product=queryMKMAPI("product/".$card->idProduct);
-            $articles=queryMKMAPI("articles/".$card->idProduct);
+            $product=queryMKMAPI("product/".$card->id);
+            $articles=queryMKMAPI("articles/".$card->id);
             $price=$product->product->priceGuide;
-            $cardPrice=\MkmScraper\CardPrice::create(array("id_card"=>$card->idProduct,"sell"=>$price->SELL,"low"=>$price->LOW,"lowex"=>$price->LOWEX,"lowfoil"=>$price->LOWFOIL,"avg"=>$price->AVG,"trend"=>$price->TREND,"sellers"=>sizeof($articles->article)));
+            $cardPrice=\MkmScraper\CardPrice::create(array("id_card"=>$card->id,"sell"=>$price->SELL,"low"=>$price->LOW,"lowex"=>$price->LOWEX,"lowfoil"=>$price->LOWFOIL,"avg"=>$price->AVG,"trend"=>$price->TREND,"sellers"=>sizeof($articles->article)));
 
         }
     }
