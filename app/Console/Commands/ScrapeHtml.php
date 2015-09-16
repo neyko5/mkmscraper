380915ae8e6be0c29f640e59bbd9bebbd122a95a
Jernej Lipovec <neykomir@gmail.com>
2015-09-16 10:16:49 +0200

    html scrape fix

--- app/Console/Commands/ScrapeHtml.php
+++ app/Console/Commands/ScrapeHtml.php
@@ -30,12 +30,22 @@ class ScrapeHtml extends Command
         $client = new \Goutte\Client();
         foreach(\MkmScraper\Card::all() as $card){
             $crawler = $client->request('GET','https://www.magiccardmarket.eu/Products/Singles/'.rawurlencode($card->set).'/'.rawurlencode($card->name));
-            $available=$crawler->filter('.sectioncontent .availTable .row_0  .cell_0_1')->first()->text();
-            $low=$crawler->filter('.sectioncontent .availTable .row_1  .cell_1_1 span')->first()->text();
-            $lowfinal=str_replace(",",".",$low);
-            $trend=$crawler->filter('.sectioncontent .availTable .row_2  .cell_2_1')->first()->text();
-            $trendnumber=explode(" ",$trend);
-            $trendfinal=str_replace(",",".",$trendnumber[0]);
+            $available=0;
+            $lowfinal=0;
+            $trendfinal=0;
+            if($av=$crawler->filter('.sectioncontent .availTable .row_0  .cell_0_1')->first()){
+                $available=$av->text();
+            }
+            if($lo=$crawler->filter('.sectioncontent .availTable .row_1  .cell_1_1 span')->first()){
+                $low=$lo->text();
+                $lowfinal=str_replace(",",".",$low);
+            }
+            if($tr=$crawler->filter('.sectioncontent .availTable .row_2  .cell_2_1')->first()){
+                $trend=$tr->text();
+                $trendnumber=explode(" ",$trend);
+                $trendfinal=str_replace(",",".",$trendnumber[0]);
+            }
+
             $cardPrice=\MkmScraper\CardPrice::create(array("id_card"=>$card->id,"low"=>$lowfinal,"trend"=>$trendfinal,"sellers"=>$available));
         }
     }
