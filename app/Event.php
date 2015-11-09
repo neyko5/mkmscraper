69f9c9aa4cb27caa83caeebfdc24d44946e4c8c2
Jernej <neykomir@gmail.com>
2015-11-09 17:06:53 +0100

    as

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $fillable=array("name","date","rank");
+    protected $fillable=array("name","date","rank","link");
 }
